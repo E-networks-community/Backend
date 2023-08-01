3a0a5e54db0a3a94841329048d23da3aedea2eaf
Aldorax <109093997+Aldorax@users.noreply.github.com>
2023-08-01 16:44:45 +0100

    Update config.py

--- config.py
+++ config.py
@@ -26,17 +26,10 @@ class ApplicationConfig:
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = False
 
-    # MAIL_SERVER = 'smtp.elasticemail.com'
-    # MAIL_PORT = 2525
-    # MAIL_USERNAME = 'coldnightdev@gmail.com'
-    # MAIL_PASSWORD = "DA79E471E994C2FBEC5BB9F44ABDF78CF139"
-    # MAIL_USE_TLS = True
-    # DATABASE_INITIALIZED = False
-
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
+    MAIL_SERVER = 'smtp.elasticemail.com'
+    MAIL_PORT = 2525
     MAIL_USERNAME = 'enetworksagencybanking@gmail.com'
-    MAIL_PASSWORD = "bfpvgghnukatybcs"
+    MAIL_PASSWORD = "E9A54FA20AD4D93955A76BEDD66A483174DD"
     MAIL_USE_TLS = True
     DATABASE_INITIALIZED = False
 
