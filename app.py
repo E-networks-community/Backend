ef0b378bf515b213a85c753389addd5286c7dc7f
Aldorax <climaxating@gmail.com>
2023-08-05 12:09:47 +0100

    chanegs

--- app.py
+++ app.py
@@ -58,7 +58,6 @@ db.init_app(app)
 def add_cors_headers(response):
     # Replace with your frontend domain
     frontend_domain = 'https://www.enetworksagencybanking.com.ng'
-    # frontend_domain = 'http://localhost:3000'
     response.headers['Access-Control-Allow-Origin'] = frontend_domain
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS, PATCH'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
